f94a4f627beb7c7be0ceca357cf99c936edab7b1
andcout01 <andcout@outlook.com>
2024-10-05 18:41:46 -0300

    biblioteca monolog

---
+++ Composer/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'andcout/composer',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ceb873fc0ae81a6365a7787c041b1cf912513432',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'andcout/composer' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ceb873fc0ae81a6365a7787c041b1cf912513432',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'monolog/monolog' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
